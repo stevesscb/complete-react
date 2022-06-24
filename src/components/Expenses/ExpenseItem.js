1025c1e67e9979bbb430900a6a1c15f8b89b1872
stevesscb <stevesscb@gmail.com>
2022-06-24 14:48:00 +0800

    module 4

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,7 +6,6 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem evaluated by React");
 
   const handleClick = () => {
     setTitle("Updated");
